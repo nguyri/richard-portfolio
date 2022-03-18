5d14f5a97322c245bc033b854c2ffb485be7c76a
Richard Nguyen <laetic@users.noreply.github.com>
2022-03-18 16:49:07 -0600

    Added webpack dev and production config settings

---
+++ webpack.common.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [ 
+        {
+          loader: 'file-loader',
+        },
+        ]
+      },
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+};
+
+// const TerserPlugin = require('terser-webpack-plugin');
+
+// module.exports = {
+//   mode: 'production',
+//   optimization: {
+//     minimizer: [new TerserPlugin({ /* additional options here */ })],
+//   },
+// };
