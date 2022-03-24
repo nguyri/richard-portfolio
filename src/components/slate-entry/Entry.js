2bdaa04b58b6a49ce317a60d305b2eb8d8e52505
Richard Nguyen <laetic@users.noreply.github.com>
2022-03-24 11:41:27 -0600

    Cleaned up transition and improved mobile styling

--- src/components/slate-entry/Entry.js
+++ src/components/slate-entry/Entry.js
@@ -8,48 +8,6 @@ import adlathe from "../../imgs/addlathe1.jpg"
 import raytracer from "../../imgs/raytracer1.jpg"
 import wordle from "../../imgs/wordle1.png"
 
-function Example() {
-    const [showButton, setShowButton] = React.useState(true);
-    const [showMessage, setShowMessage] = React.useState(false);
-    return (
-        <Container style={{ paddingTop: '2rem' }}>
-            {showButton && (
-                <Button
-                    onClick={() => setShowMessage(true)}
-                    size="lg"
-                >
-                    Show Message
-                </Button>
-            )}
-            <CSSTransition
-                in={showMessage}
-                timeout={300}
-                classNames="alert"
-                unmountOnExit
-                onEnter={() => setShowButton(false)}
-                onExited={() => setShowButton(true)}
-            >
-                <Alert
-                    variant="primary"
-                    dismissible
-                    onClose={() => setShowMessage(false)}
-                >
-                    <Alert.Heading>
-                        Animated alert message
-                    </Alert.Heading>
-                    <p>
-                        This alert message is being transitioned in and
-                        out of the DOM.
-                    </p>
-                    <Button onClick={() => setShowMessage(false)}>
-                        Close
-                    </Button>
-                </Alert>
-            </CSSTransition>
-        </Container>
-    );
-}
-
 export default function Entry(props) {
     // eventually change this to webpack import
     const imgList = [mpcnc, adlathe, raytracer, wordle]
@@ -57,14 +15,13 @@ export default function Entry(props) {
 
     function smallEntry() {
         return (
-            <div className={props.darkMode ? "entry entry--dark" : "entry"}>
+            <div className={props.darkMode ? "entry entry--dark" : "entry"} onClick={() =>setEntryIsSmall(!entryIsSmall)}>
                 <img src={imgList[props.imageNum]} className="entry--img" />
                 {/* <div className="overlay"> */}
                 <div className="entry--col">
                     <h1 className={props.darkMode ? "entry--title entry--dark" : "entry--title"}> {props.title} </h1>
                     <p className={props.darkMode ? "entry--text entry--dark" : "entry--text"}> {props.description} </p>
                 </div>
-
             </div>
         )
     }
@@ -78,18 +35,12 @@ export default function Entry(props) {
 
     function bigEntry() {
         return (
-            <div className={props.darkMode ? "entry entry--dark" : "entry"}>
-                <img src={imgList[props.imageNum]} className="entry--img" style={{ width: "20vw" }} />
+            <div className={props.darkMode ? "entry entry--dark" : "entry"} onClick={() =>setEntryIsSmall(!entryIsSmall)}>
+                <img src={imgList[props.imageNum]} className="entry--img entry--img-big" />
                 <div className="entry--col">
                     <h1 className={props.darkMode ? "entry--title entry--dark" : "entry--title"}> {props.title} </h1>
                     {paragraphBigEntry(props.longdescription)}
                 </div>
-                <Button
-                    onClick={() => setEntryIsSmall(!entryIsSmall)}
-                    size="lg"
-                >
-                Show Less
-                </Button>
             </div>
         )
     }
@@ -97,24 +48,18 @@ export default function Entry(props) {
     return (
         <div>
             {entryIsSmall && smallEntry()}
-            {entryIsSmall && (
-                <Button
-                onClick={() => setEntryIsSmall(!entryIsSmall)}
-                size="lg"
-            >
-                Show More
-            </Button>
-            )}
             <CSSTransition
                 in={!entryIsSmall}
-                timeout={300}
+                timeout={{
+                    appear:300,
+                    enter:300,
+                    exit:0,}}
                 classNames="alert"
                 unmountOnExit
                 onEnter={() => setEntryIsSmall(false)}
                 onExited={() => setEntryIsSmall(true)}
             >
                 {bigEntry()}
-
             </CSSTransition>
 
         </div>
