6ef4ba72566f397cb91b749fcbc29341f631beb8
Richard <laetic@users.noreply.github.com>
2022-04-04 17:03:24 -0600

    Added multiple model loader

--- src/components/threejsdemo/Three.js
+++ src/components/threejsdemo/Three.js
@@ -25,12 +25,30 @@ importAll(require.context('../../models/', false, /\.(3mf)$/));
 
 export default class ThreeScene extends Component{
 
+  loadThreeMF(loader, paths, list, scene) {
+    // loader.addExtension( ThreeMFLoader.MaterialsAndPropertiesExtension );
+
+    paths.forEach( path => {
+      loader.load(models[path].default, ( object3mf ) => {
+        // console.log(object3mf)
+        list.push(object3mf);
+        object3mf.children[0].children[0].material=new THREE.MeshDepthMaterial();
+        // object3mf.children[0].children[0].material=new THREE.MeshStandardMaterial();
+        scene.add(object3mf);
+      }, undefined, function ( error ) {
+        console.error( error );
+      } );
+    })
+
+  }
+
+
   componentDidMount(){
     this.translation = 0; 
     const width = this.mount.clientWidth
     const height = this.mount.clientHeight
     this.scene = new THREE.Scene()
-    this.camera = new THREE.PerspectiveCamera( 25, width / height, 120, 200)
+    this.camera = new THREE.PerspectiveCamera( 25, width / height, 10, 500)
     this.camera.up.set(0,0,1);
     // this.camera = new THREE.PerspectiveCamera( 35, window.innerWidth / window.innerHeight, 1, 500 );
 
@@ -39,7 +57,8 @@ export default class ThreeScene extends Component{
     this.scene.add( this.pointLight );
     this.scene.add( new THREE.AmbientLight( 0xffffff, 0.6 ) );
 
-    this.camera.position.set( 80, 80, 150 );
+    this.camera.position.set( 106, 132, 84 );
+    this.camera.rotation.set(-1.15,0.499, 2.93)
     //ADD RENDERER
     this.renderer = new THREE.WebGLRenderer({ antialias: true })
     this.renderer.setClearColor('#999999')
@@ -53,15 +72,9 @@ export default class ThreeScene extends Component{
 
     this.object3mf = {}
     let loader = new ThreeMFLoader();
-    // loader.addExtension( ThreeMFLoader.MaterialsAndPropertiesExtension );
-    loader.load(models['./lamp-base-1.3mf'].default, ( object3mf ) => {
-      console.log(object3mf)
-      this.object3mf = object3mf;
-      this.object3mf.children[0].children[0].material=new THREE.MeshDepthMaterial();
-      this.scene.add( this.object3mf);
-    }, undefined, function ( error ) {
-      console.error( error );
-    } );
+    let modelPaths = ['./lamp-base-1.3mf','./lamp-clamp-2.3mf', './lamp-bolt-1.3mf', './lamp-finger-nut-1.3mf']
+    this.modelList = []
+    this.loadThreeMF(loader, modelPaths, this.modelList, this.scene)
 
     let controls = new OrbitControls( this.camera, this.renderer.domElement );
     controls.addEventListener( 'change', this.render );
@@ -99,12 +112,21 @@ renderScene = () => {
 
 setTranslation = (num) => {
   this.translation = num;
-  this.object3mf.position.z = num;
+  // console.log(this.modelList);
+  let unitArray = []
+  for(var i = this.modelList.length - 1; i >= 0; i--) {
+    let step = 2/(this.modelList.length - 1)
+    unitArray.push( step * i - 1)
+  }
+
+  console.log(unitArray);
+  this.modelList.forEach( (model, index) => {
+    model.position.z = num * unitArray[index]
+  })
   // console.log(this.translation);
 }
 
 render(){
-
     return(
       <div
         style={{ width: '400px', height: '400px' }}
