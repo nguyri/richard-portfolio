ff756efe6a860111d8fa445278e8c1cdf67cfbf6
Richard Nguyen <11547131+nguyri@users.noreply.github.com>
2022-09-12 15:13:04 -0600

    Add more writing content to each page

--- src/components/entry/data.js
+++ src/components/entry/data.js
@@ -49,6 +49,7 @@ let entries = [
             {key: 'electronics-setup', subtitle:`Electronics Setup`},
             {key:nanoid(), text:`For electronics, an Arduino Uno running GRBL with four TB6600 stepper modules turns the g-code into stepper motor power. A raspberry pi 3 running cnc.js
             provides a great way to preview and control GRBL and load g-code over wifi.`},
+            {key:nanoid(), imageName:'./welding2.jpg', style: {marginBottom:"20px", gridRow:"span 5", overflow:"hidden", height:"250px"}},
             {key:7, text:`Unfortunately this setup was plagued by electronics issues for a while. A combination of poor connectors and very strong 
             electromagnetic interference (EMI) took a lot of troubleshooting. After crimping an endless number of 2.54mm 'dupont' connectors, I replaced them all with locking JST-SM ones.`},
             {key:8, text:`Reducing EMI meant rewiring the entire CNC machine, shielding to all the exterior cables, adding an enclosure for the controllers, 
@@ -57,11 +58,9 @@ let entries = [
             buy a blowback plasma from the start.`,},
             {key: 'upgrading-the-controller', subtitle:`Upgrading the Controller`},
             {key:nanoid(), text:`The raspberry pi and arduino are more suitable as prototyping tools and not particularly noise-resistant. I had relatively few faults with my raspberry pi 3 running 
-            cnc.js, however the arduino often reset itself when a plasma arc was started.`,},
+            cnc.js, however the arduino often reset itself when a plasma arc started.`,},
             {key:nanoid(), text:`Options exist for "industrial" arduinos. It's also possible to get a very cheap Mach3 controller with ethernet connection. 
             The ethernet connection offers a huge bonus with error detection and correction, so I opted for the inexpensive NVEM2 controller.`,},
-            {key:nanoid(), text:`The control channels are all photo-isolated as well. I ended up putting current limiting resistors for all channels. The documentation makes
-            no mention of the exact electronics so I put them in to be safe.`,},
             {key: 'mach3', subtitle:`Mach3`},
             {key:nanoid(), text:`Perhaps you've lived a post Web2.0 life, in a garden of minimalist, rounded cornered responsive webpages. Mach3 revives the geocities era
              into its eye-squintingly cluttered user interface. It's a small price to pay for the reliability of an ethernet connection however.`,},
@@ -76,6 +75,7 @@ let entries = [
             {key: 'pierce-timing', subtitle:`Pierce Timing`},
             {key:nanoid(), text:`I had problems with getting a pierce delay of under 1 second, which resulted in a splattery pierce hole that's larger than the rest of the cut. It's possible to 
             pierce outside the part and then bring the tool to the cut line so I didn't spend too long on it.`},
+            {key: nanoid(), vimeo:'https://vimeo.com/675640978', }, 
             {key: 'welding', title:`Welding` },
             {key:nanoid(), text:`Welding is easy to learn and hard to master. I quite liked Steve Bleile’s welding videos. They are densely packed with information and without the 
             embellishments of a youtube tutorial.`},
@@ -141,8 +141,32 @@ let entries = [
             {key: 16, vimeo:'https://vimeo.com/692393001', style: {}},
             {key: 3, text:`I designed the cylindrical slicer and also integrated it into a popular open source slicer, Cura. 
             I also rewrote part of the display shader to display print previews with cylindrical layers.`, style: {gridRow:"span 5"}},
+            {key: 'slicermodifications', title:'Slicer Modifications'},
+            {key: nanoid(), text:`The goal of the slicer is to create toolpaths that recreate the 3D model in cylindrical coordinates.
+            This differs from traditional cartesian printers because the X axis is replaced with a rotating (theta or B) axis. 
+            There are many new challenges to slicing cylindrically, particularly due to the layer geometry changing from flat 
+            planes to round tubes.`},
+            {key:5, img:'test img', style: {gridColumn:"span 5", marginBottom: "30px", height:"300px"}, className:"entry--img-inline", imageName:'./addlathe2.jpg'},
+            {key: 'geometrymodifications', subtitle:'Geometry Modifications'},
+            {key:nanoid(), text:`Here a cylinder with a window is sliced cylindrically. Note that the window remains perpendicular 
+            to the drum, so it's an overhanging feature due to the smaller angular size as the layers increase in radius.`},
+            {key:nanoid(), text:`Once the model is in an STL file the cartesian coordinates are 'baked in'. That is, decoding 
+            the STL file gives you a list of XYZ coordinates and not angle, radius and height dimensions. It was eventually decided
+            that writing the cartesian to cylindrical converter inside the slicer would be the best course of action. `},
+            {key:nanoid(), text:`For simplicity, a seam was added from 0 to pi so each layer was a "closed shape". You can see the 
+            artifact in this is an animation of one of the infill layers. The slicer is unsatified when an open shape is created
+            when slicing because this is usually an input geometry fault. However, two separate rings can make an enclosed shell
+            in cylindrical coordinates, that would simply be a cylinder. This feature was avoided for the time being.`,},
+            {key:nanoid(), text:``,},
+            {key:nanoid(), text:``,},
+            {key: 'hardware', title:'Hardware'},
+            {key:nanoid(), text:`The project is based on the excellent Zaribo upgrade for Prusa MK2 printers. This upgrade replaces
+            the sheet panel frame with aluminum extrusion, as well as directly coupled Z lead screws and other organizational
+            improvements. `,},
+            {key:nanoid(), text:`The frame is paired with a Duet 2 control board, which has an excellent user interface, ethernet 
+            connection, and dual extruder outputs.`,},
             {key: 2, text:`The project won first place in the 2019 Schulich School of Engineering Capstone Fair as well as 2019 CSME National Design Competition Best Overall Design.`, style: {gridRow:"span 5"}},
-            {key:5, img:'test img', style: {gridColumn:"span 5", margin: "20px 20px 20px 0px "}, className:"entry--img-inline", imageName:'./addlathe2.jpg'},
+            
         ],
         imageName:'./addlathe1.jpg',
         imageStyle:{style: {objectPosition:'right 0 bottom 200px'}}
@@ -161,8 +185,8 @@ let entries = [
             User inputs such as playing wordle!</div>},
             {key: 3, wordle: true},
             {key: 2, text:`This website is written in Javascript and CSS with some great packages. Some of the more popular ones 
-            are react, react-router, threejs, and a touch of bootstrap. 
-            On the backend it's built with babel and webpack. Deployment is on an nginx server running on an arch VPS.`},
+            are react, react-router, and react-three-fiber. 
+            On the backend it's built with babel and webpack. Deployment is an nginx server running on an arch VPS.`},
         ],
         imageName:'./wordle2.png',
     },
@@ -180,14 +204,16 @@ let entries = [
             {key: 16, vimeo:'https://vimeo.com/612194957', style: {}},
             {key: 2, text:`Admittedly, using this small garage router is more finnicky than using a rigid commercial machine
             with a spindle measured in horsepower. One upside is that if you crash it, the tiny steppers can't possibly generate enough force to damage anything other than your self esteem.`}, 
+            {key:'commontroubleshooting', subtitle:'Common Troubleshooting'},
             {key:5, img:'test img', style: {gridColumn:"span 3", gridRow:"span 3", marginBottom:"30px"}, className:"entry--img-inline",  imageName:'./mpcnc2.jpg'},
             {key: 4, text:`The most common issue was with electrical connectors, which were simple but painstaking to fix. Generic 'dupont' or 0.1mm connectors
             will do the job for breadboarding, but for constant motion dedicated wire to wire or wire to panel connectors are better. `, style: {gridColumn:"span 2"}},
-            {key: 6, text:`Occasionally a tool would catch a wall it was not meant to. The full engagement of the 
-            flutes could overpower the machine, making the tool dive. This was easy enough to fix in the CAM process, by
-            keeping away from the wall until a light finishing pass.`, style: {gridColumn:"span 2"}},
-            {key: 3, text:`At the end of this project I found myself not using the router particularly often because routing
-            wood was messy and took a long time. However it has turned into my CNC plasma cutter which seems like a better fit for this machine.`, style: {gridColumn:"span 5"}}
+            {key: 6, text:`Occasionally a tool would catch a wall it was not meant to. Cutting with the full cutter could overpower the steppers and making the tool dive. This could be fixed in CAM, by keeping away from the wall until a light finishing pass.`, style: {gridColumn:"span 2"}},
+            {key:'laser', title:'Laser', style:{marginTop:'30px'}},
+            {key: nanoid(), text:`At the end of this project I found myself not using the router particularly often because routing
+            wood was messy and takes a long time. `},
+            {key: nanoid(), text:`The laser is simpler to setup than plasma, requiring only power and optional air assist. Unfortunately, paper or plastic are most suitable for the laser cutter which smell terrible when vaporized. Even running it in an open garage it is quite stinky. `},
+            {key: nanoid(), vimeo:'https://vimeo.com/612194957', style: {}},
         ],
         
         imageName:'./mpcnc1.jpg',
@@ -201,14 +227,32 @@ let entries = [
         longdescription: [
             {key:'woodshop', title:`Woodshop`},
             {key: 1, text:`I’ve been using wood tools for a very long time. 
-            I started off really rather bad at making stuff out of wood. Nowadays I can make some things passably. 
+            I started off rather bad at making stuff out of wood. Nowadays I do alright. 
             I find making your own things gives you new and better appreciation for your surroundings.`},
-            {key: 2, subtitle:`Chessboard`},
+            {key: 'safety', subtitle:`Safety`},
+            {key:nanoid(), text:`Starting with woods requires a healthy fear of wood tooling. Wood tools move faster and store more energy than (equivalently sized) metal ones due to the higher RPM needed to sever wood fibers.`},
+            {key:nanoid(), html:<div>Kickback occurs when the cutter enters the material in an uncontrolled manner, usually throwing or destroying the workpiece. In a 2011 report by the CPSC, <a href="https://cpsc.gov/s3fs-public/statsaws.pdf">available at this link</a>, nearly 80000 injuries occuried in the US between 2007 and 2009. </div>},
+            {key:nanoid(), text:`Don't do anything sketchy on the table saw. When you do end up doing something sketchy, stop moving and turn off the saw until is fully stops.`},
+            {key: 'woodgrain', subtitle:`Wood Grain`},
+            {key:nanoid(), text:`The next fundametal of wood is working with the grain. Most people start off with a mitre or table saw which are in fact, fairly grain agnostic.`},
+            {key:nanoid(), text:`The jointer and planer prefer going down grain however, and if you're ever hand-planed a piece of wood against the grain you'll know it's difficult and by ripping out groups of fibers it ruins the flatness of the board. `},
+            {key:nanoid(), text:`Take some time to examine the grain of your wood, it'll save you time and effort later on.`},
+            {key: 'flatandsquare', subtitle:`Flat and Square`},
+            {key:6, text:`Once you equip your shop with a jointer, planer, table and mitre saw you can make square and flat wood safely and with ease. Flat and square wood is required to get consistent results`},
+            {key:nanoid(), text: `It does take quite a few steps. Usually, crosscut to an easy-to-handle size, oversize rip, jointer, planer, rip and crosscut again to the final size. `},
+            {key:nanoid(), html: <div>
+                The initial crosscut has a few benefits. 
+                <ul><li>
+                If you try to stick an 8 foot piece of wood into your 12.5" planer you
+                 will probably knock something important over.</li>
+                <li>For bowed pieces, it will also reduce the amount of relative bowing from end to end. A heavily bowed 8' piece of wood might be so bowed that there really is no 'flat wood' inside of there. Reduce it to 2 feet and you'll have to do only a quarter of the flattening.</li>
+                    </ul>
+            </div>},
+            {key:7, text:`Final size is often subjective. Wood furniture is usually static or only needs simple holes for metal fasteners. Most of the time I cut wood to the width of another piece of wood, without any especial care to the exact size. Wood also bends and warps over time anyways. `},
+            {key: 'chessboard', title:`Chessboard`},
             {key:5, img:'test img', style: {gridColumn:"span 2", gridRow:"span 4", marginBottom:"20px",}, className:"entry--img-inline", imageName: './wood2.jpg'},
-            {key:4, text:`I decided to do this chessboard out of red oak and black walnut and I was quite pleased with it. I haven’t done many projects 
-            out of hardwood since I haven’t had a jointer and planer until recently.`, style: {gridColumn:"span 3"}},
-            {key:6, text:`It's really satisfying to square up your stock. It is quite a process however. Usually: Crosscut to an easy to handle size, oversize rip to within 
-            quarter inch, jointer, planer, rip and crosscut again to the final size.`, style: {gridColumn:"span 3", marginBottom:"20px",}},
+            {key:4, text:`I decided to do this chessboard out of red oak and black walnut and I was quite pleased with it. At the 
+            time of writing I haven’t done many projects out of hardwood and have only recently gotten a jointer and planer.`, style: {gridColumn:"span 3"}},
             {key:7, img:'test img', style: {gridColumn:"1 / span 2",  marginBlock:"20px", height:'40vh'}, className:"entry--img-inline", imageName:'./wood3.jpg' },
             {key:8, img:'test img', style: {gridColumn:"span 3",  marginBlock:"20px", height:'40vh'}, className:"entry--img-inline", imageName:'./wood1.jpg' },
             {key:9, img:'test img', style: {gridColumn:"1 /span 2",  marginBottom:"20px", height:'40vh'}, className:"entry--img-inline", imageName:'./wood5.jpg' },
