12ae0ceef33e54235f0af8da1810d137cd02b6ad
Richard Nguyen <11547131+nguyri@users.noreply.github.com>
2023-06-15 16:27:45 -0600

    Resized BlendingModes to use row span

--- src/components/blendingmodes/BlendingModes.js
+++ src/components/blendingmodes/BlendingModes.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Slider from 'rc-slider'
 
 
-export default function BlendingModes() {
+export default function BlendingModes(props) {
     const [slider, setSlider] = React.useState(() => 1);
     
     return (
-        <div style={{ width:"300px"}}>
+        <div style={props.style}>
           Value:{slider}
           <Slider onChange={setSlider} min={1} max={30} />
         </div>
