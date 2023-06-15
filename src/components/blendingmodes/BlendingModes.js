2d605836e3bd6c0e64eb8ef621737ece2f483d12
Richard Nguyen <11547131+nguyri@users.noreply.github.com>
2023-06-15 16:11:56 -0600

    Resized slider

--- src/components/blendingmodes/BlendingModes.js
+++ src/components/blendingmodes/BlendingModes.js
@@ -4,10 +4,11 @@ import Slider from 'rc-slider'
 
 export default function BlendingModes() {
     const [slider, setSlider] = React.useState(() => 1);
+    
     return (
-        <div>
-          <Slider onChange={setSlider} min={1} max={30} />
+        <div style={{ width:"300px"}}>
           Value:{slider}
+          <Slider onChange={setSlider} min={1} max={30} />
         </div>
     )
 }
