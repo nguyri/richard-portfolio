aea5e8e995a7db7cd5a899235e38e9c9465da07d
Richard Nguyen <11547131+nguyri@users.noreply.github.com>
2023-06-21 09:21:20 -0600

    Added a custom filter, still doesn't respond to slider

--- src/components/blendingmodes/BlendingModes.js
+++ src/components/blendingmodes/BlendingModes.js
@@ -35,17 +35,32 @@ export default function BlendingModes(props) {
 
   React.useEffect( () => {
     if(image) {
-      console.log(imageRef.current);
       imageRef.current.cache();
     }
-  }, [image, slider])
+    // return () => {
+    //   imageRef.current.cache();
+    // }
+  // }, )
+  }, [image])
 
   var MultiplyFilter = function (imageData) {
     var nPixels = imageData.data.length;
     for (var i = 3; i < nPixels; i += 4) {
-      imageData.data[i] *= 2;
+      imageData.data[i] *= slider;
     }
   };
+  // lets define a custom filter:
+var ColorReplaceFilter = function (imageData) {
+  var nPixels = imageData.data.length;
+  for (var i = 0; i < nPixels - 4; i += 4) {
+    const isDark = imageData.data[i] + imageData.data[i+2] + imageData.data[i+3] < 600;
+    if (isDark) {
+      imageData.data[i] = 90;
+      imageData.data[i + 1] = 149;
+      imageData.data[i + 2] = slider;
+    }
+  }
+};
 
   const ColoredRect = () => {
 
@@ -58,7 +73,7 @@ export default function BlendingModes(props) {
       <Rect x={0} y={0} width={200} height={80} cornerRadius={5} fillPatternImage={image}
       fillPatternScale={{x:0.3,y:0.3}} fillPatternRepeat='no-repeat' // opacity={(256 - slider)/255}
       //  shadowBlur={5} shadowColor={"#eeeeee"} onClick={handleClick} margin = {10}
-      filters={[Konva.Filters.Blur]} blurRadius={slider}
+      filters={[ColorReplaceFilter]} blurRadius={slider}
       ref={imageRef}
       />
     );
