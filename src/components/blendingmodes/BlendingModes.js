d66a16ad380aab7ce58f392b204f96a9e27cef60
Richard Nguyen <11547131+nguyri@users.noreply.github.com>
2023-06-21 17:03:14 -0600

    Styling on multiply blend mode

--- src/components/blendingmodes/BlendingModes.js
+++ src/components/blendingmodes/BlendingModes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Slider from 'rc-slider'
 import { Stage, Layer, Rect, Text, Image } from 'react-konva';
-import {MathJax, MathJaxContext } from 'better-react-mathjax'
+import { MathJax, MathJaxContext } from 'better-react-mathjax'
 import Konva from 'konva';
 import { getEntry, getImage } from '../entry/data'
 import useImage from 'use-image';
@@ -9,11 +9,11 @@ import useImage from 'use-image';
 
 export default function BlendingModes(props) {
   const baseEqn = "\\(f(a,b) = ab";
-  const [slider, setSlider] = React.useState(() => 1);
-  const [color, setColor] = React.useState('green');
-  const [eqn, setEqn] = React.useState(baseEqn + '\\)');
   const [image] = useImage(getImage('./art1.jpg'));
   const imageRef = React.useRef();
+  const [slider, setSlider] = React.useState(() => 256);
+  const [color, setColor] = React.useState('white');
+  const [eqn, setEqn] = React.useState(baseEqn + '\\)');
   const config = {
     loader: { load: ["input/asciimath"] },
     asciimath: {
@@ -24,27 +24,25 @@ export default function BlendingModes(props) {
       ]
     }
   };
-  
+  const width = 400, height = 200, heightFraction = 5;
 
-  // React.useEffect( () => {
-  //   let change = slider;
-  //   let colorStr = 'rgb(' + change + ',255,0)';
-  //   setColor(colorStr);
-  //   setEqn(baseEqn + ' test ' + slider + '\\)');
-  // }, [slider])
-
-  React.useEffect( () => {
-    if(image) {
+  React.useEffect(() => {
+    if (image) {
       imageRef.current.cache();
     }
-  }, [slider])
+
+    let colorStr = `rgb(${slider},${slider},${slider})`;
+    // let colorStr = 'blue';
+    console.log(colorStr);
+    setColor(colorStr);
+  }, [slider, color])
 
   var MultiplyFilter = function (imageData) {
     var nPixels = imageData.data.length;
     for (var i = 0; i < nPixels - 4; i += 4) {
-      imageData.data[i] *= slider / 120; // red
-      imageData.data[i + 1] *= slider / 120; // green
-      imageData.data[i + 2] *= slider / 120; // blue
+      imageData.data[i] *= slider / 256; // red
+      imageData.data[i + 1] *= slider / 256; // green
+      imageData.data[i + 2] *= slider / 256; // blue
     }
   };
 
@@ -55,32 +53,36 @@ export default function BlendingModes(props) {
       // setColor(color);
     };
 
-    return (
-      <Rect x={0} y={0} width={200} height={80} cornerRadius={5} fillPatternImage={image}
-      fillPatternScale={{x:0.3,y:0.3}} fillPatternRepeat='no-repeat' // opacity={(256 - slider)/255}
-       shadowBlur={5} shadowColor={"#eeeeee"} onClick={handleClick} margin = {10}
-      filters={[MultiplyFilter]} 
-      ref={imageRef}
-      />
-    );
+    return;
   };
-  
+
   return (
     <div style={props.style}>
-      <MathJaxContext config={config}>
-      <MathJax >{`$f(a,b) = a * b$`}</MathJax>
-        <MathJax inline dynamic>
-      <Stage width={200} height={80} >
-        <Layer>
-          {/* <Image image={image}/> */}
-          {/* <Rect x={0} y={0} width={200} height={80} cornerRadius={5} fill={color} shadowBlur={5} shadowColor={"#eeeeee"} margin = {10}/> */}
-          <ColoredRect />
-        </Layer>
-      </Stage>
-            <span >{`$f(a,${slider}) = a * ${slider}$`}</span>
+      <div style={{ marginBlock: "30px", display: "flex", flexDirection: "column" }}>
+        <MathJaxContext config={config}>
+          <MathJax style={{ display: "inlineblock", background: "lightgrey", borderRadius: "5px", padding: "8px", marginBlock: "10px", scale: "1.2", transformOrigin: "left top" }}>
+            <span style={{ display: "flex", flexDirection: "row" }}>{`$f(a,b) = a * b//256 = a * hat b$`}</span>
+          </MathJax>
+          <Stage width={width} height={height} cornerRadius={20}>
+            <Layer >
+              {/* <Image image={image}/> */}
+              <Rect x={0} y={0} width={width / 2} height={height / heightFraction} cornerRadius={0}
+                fillPatternImage={image} fillPatternScale={{ x: 0.4, y: 0.4 }} fillPatternOffsetY={130} />
+              <Rect x={height} y={0} width={width / 2} height={height / heightFraction} cornerRadius={0} fill={color} />
+              <Rect x={0} y={height/heightFraction} width={width} height={height} cornerRadius={0} fillPatternImage={image}
+                fillPatternOffsetY={50} fillPatternScale={{ x: 0.6, y: 0.6 }} fillPatternRepeat='no-repeat'
+                //  shadowBlur={5} shadowColor={"#eeeeee"} onClick={handleClick} margin = {10}
+                filters={[MultiplyFilter]}
+                ref={imageRef} />
+            </Layer>
+          </Stage>
+          <MathJax inline dynamic style={{ background: "lightgrey", borderRadius: "5px", padding: "8px", marginBlock: "10px", scale: "1.2", transformOrigin: "left top" }}>
+            <span >{`$f(a,\color{${color}}${slider}) = a * \color{${color}} ${((slider) / 256).toFixed(2)}$`}</span>
           </MathJax>{" "}
-      </MathJaxContext>
-      <Slider onChange={setSlider} min={1} max={255} defaultValue={120} step={1}/>
+        </MathJaxContext>
+        <Slider onChange={setSlider} reverse={true} min={1} max={255} defaultValue={255} step={1} 
+        style={{ paddingBlock: '10px', scale: "2", transformOrigin: "left top", width:"25%" }} />
+      </div>
     </div>
   )
 }
