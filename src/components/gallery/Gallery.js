7bef7b1c0920bd8973a80a76445b0f9ee5558738
Richard Nguyen <11547131+nguyri@users.noreply.github.com>
2023-06-27 13:43:44 -0600

    Added image detail styling

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -12,16 +12,29 @@ export default function Gallery (props) {
     const [expandID, setExpandID] = React.useState(-1);
     let rowCounter = 0, row = 0;
     const images = [
-        {id:"vest", path: "./art1.jpg", backgroundSize:"110%", backgroundPosition:"20% 0%"},
+        {id:"vest", path: "./art1.jpg", backgroundSize:"110%", backgroundPosition:"20% 0%", detailSize:"100%", detailPosition:"50% 20%"},
+        {id:"miku",  path: "./art4.jpg", backgroundSize:"170%", backgroundPosition:"45% 5%", detailSize:"100%", detailPosition:"50% 10%"},
+        {id:"lucy",  path: "./art11.jpg", backgroundSize:"180%", backgroundPosition:"50% 40%", detailSize:"120%", detailPosition:"50% 30%"},
+        {id:"wires",  path: "./art2.jpg", backgroundSize:"150%", backgroundPosition:"40% 40%", detailSize:"100%", detailPosition:"50% 30%"},
+        {id:"ninja", path: "./art6.jpg", backgroundSize:"150%", backgroundPosition:"50% 00%", detailSize:"100%", detailPosition:"50% 10%"},
+        // {id:"hot", path: "./art7.jpg", backgroundSize:"250%", backgroundPosition:"50% 50%", detailSize:"100%", detailPosition:"50% 50%"},
+        {id:"kick",  path: "./art8.jpg", backgroundSize:"120%", backgroundPosition:"40% 20%", detailSize:"100%", detailPosition:"50% 20%"},
+        {id:"sword",  path: "./art9.jpg", backgroundSize:"120%", backgroundPosition:"50% 50%", detailSize:"100%", detailPosition:"50% 30%"},
+        {id:"updo",  path: "./art5.jpg", backgroundSize:"140%", backgroundPosition:"50% 50%", detailSize:"100%", detailPosition:"50% 30%"},
+        {id:"coat", path: "./art10.jpg", backgroundSize:"160%", backgroundPosition:"50% 20%", detailSize:"100%", detailPosition:"50% 30%"},
+    ]
+
+    const imageDetails = [
+        {id:"vest", path: "./art1.jpg", backgroundSize:"200%", backgroundPosition:"20% 0%"},
         {id:"wires",  path: "./art2.jpg", backgroundSize:"200%", backgroundPosition:"40% 40%"},
-        {id:"miku",  path: "./art4.jpg", backgroundSize:"170%", backgroundPosition:"45% 5%"},
-        {id:"updo",  path: "./art5.jpg", backgroundSize:"140%", backgroundPosition:"50% 50%"},
-        {id:"ninja", path: "./art6.jpg", backgroundSize:"150%", backgroundPosition:"50% 00%"},
-        // {id:"hot", path: "./art7.jpg", backgroundSize:"250%", backgroundPosition:"50% 50%"},
-        {id:"kick",  path: "./art8.jpg", backgroundSize:"120%", backgroundPosition:"40% 20%"},
-        {id:"sword",  path: "./art9.jpg", backgroundSize:"120%", backgroundPosition:"50% 50%"},
-        {id:"lucy",  path: "./art11.jpg", backgroundSize:"180%", backgroundPosition:"50% 40%"},
-        {id:"coat", path: "./art10.jpg", backgroundSize:"160%", backgroundPosition:"50% 20%"},
+        {id:"miku",  path: "./art4.jpg", backgroundSize:"200%", backgroundPosition:"45% 5%"},
+        {id:"updo",  path: "./art5.jpg", backgroundSize:"200%", backgroundPosition:"50% 50%"},
+        {id:"ninja", path: "./art6.jpg", backgroundSize:"200%", backgroundPosition:"50% 00%"},
+        // {id:"hot", path: "./art7.jpg", backgroundSize:"200%", backgroundPosition:"50% 50%"},
+        {id:"kick",  path: "./art8.jpg", backgroundSize:"200%", backgroundPosition:"40% 20%"},
+        {id:"sword",  path: "./art9.jpg", backgroundSize:"200%", backgroundPosition:"50% 50%"},
+        {id:"lucy",  path: "./art11.jpg", backgroundSize:"200%", backgroundPosition:"50% 40%"},
+        {id:"coat", path: "./art10.jpg", backgroundSize:"200%", backgroundPosition:"50% 20%"},
     ]
 
     const handleScroll = () => {
--- src/components/gallery/ImageDetail.js
+++ src/components/gallery/ImageDetail.js
@@ -6,8 +6,8 @@ export function ImageDetail (props) {
     // console.log(props);
     const elem = props.props;
     return (
-        <div key={elem.id} style={{backgroundImage:`url(${getImage(elem.path)})`, backgroundSize:elem.backgroundSize,
-            backgroundPosition:elem.backgroundPosition, gridRow:`${elem.row} / span 1`}} onClick={elem.handleClick} className={elem.className}>
+        <div key={elem.id} style={{backgroundImage:`url(${getImage(elem.path)})`, backgroundSize:elem.detailSize,
+            backgroundPosition:elem.detailPosition, gridRow:`${elem.row} / span 1`}} onClick={elem.handleClick} className={elem.className}>
         </div>
     );
 }
