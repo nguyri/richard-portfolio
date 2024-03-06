60ff303b7c60a00eb1e67589501228fd6d1ad269
Richard Nguyen <11547131+nguyri@users.noreply.github.com>
2024-03-06 11:30:29 -0700

    Fix table of contents highlighting

--- src/components/entry/TableOfContents.js
+++ src/components/entry/TableOfContents.js
@@ -36,7 +36,7 @@ const useIntersectionObserver = (setActiveId) => {
     const observer = new IntersectionObserver(callback, {
         // rootMargin: '-110px',
         threshold:1.0,
-        rootMargin: '-110px 0px -60% 0px',
+        rootMargin: '-40px 0px -60% 0px',
     });
 
     const headingElements = Array.from(document.querySelectorAll("h2, h3"));
