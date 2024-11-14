6675ec1941096442c282c23e4969de809f812a3b
Richard Nguyen <11547131+nguyri@users.noreply.github.com>
2024-11-14 15:56:11 -0700

    Added mahjong unicode

--- src/components/express/ExpressDemo.js
+++ src/components/express/ExpressDemo.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 
-export function validTile (tile) {
+function validTile (tile) {
   let validSuit = ['b','c','m', 'd', 'w']
   let validDragon = ['r','w','g']
   let validWind = ['n','e','s','w']
@@ -15,15 +15,50 @@ export function validTile (tile) {
   return 0 < value && value < 10;
 }
 
-export function validHand (handStr) {
+function validHand (handStr) {
   // valid format is b1b2b3c1c2c3m1m2m3drdrdrdgdg, 4 melds one pair. Start with making sure tiles are valid
   if (handStr.length != 28) return false;
-  const tiles = [];
-  for (let i = 0; i < handStr.length; i+=2) {
-    tiles.push(handStr.slice(i, i + 2));
-  }
+  const tiles = hand(handStr);
+  console.log(tiles);
   let allValid = tiles.every(tile => validTile(tile));
   return allValid;
+}
+
+function getMahjongUnicode(tile) {
+  const suit = tile.charAt(0); 
+  if (!validTile) return;
+  tile = tile.replace('dg', 'd1');
+  tile = tile.replace('dr', 'd2');
+  tile = tile.replace('dw', 'd3');
+  tile = tile.replace('wn', 'w1');
+  tile = tile.replace('we', 'w2');
+  tile = tile.replace('ws', 'w3');
+  tile = tile.replace('ww', 'w4');
+  const tileNumber = parseInt(tile.charAt(1)); 
+  console.log(tile);
+  // Unicode mapping for Mahjong tiles
+  let unicode = '';
+
+  switch (suit) {
+    case 'b': // Bamboo tiles (b1 - b9)
+      unicode = String.fromCodePoint(0x1F000 + (tileNumber - 1)); 
+      break;
+    case 'c': // Character tiles (c1 - c9)
+    unicode = String.fromCodePoint(0x1F00A + (tileNumber - 1));
+      break;
+    case 'm': // Man tiles (m1 - m9)
+    unicode = String.fromCodePoint(0x1F012 + (tileNumber - 1));
+      break;
+    case 'd': // Dragon tiles (d1 - d3)
+      unicode = String.fromCodePoint(0x1F013 + (tileNumber - 1));
+    case 'w': // Wind tiles (w1 - w4)
+      unicode = String.fromCodePoint(0x1F016 + (tileNumber - 1));
+    default:
+      console.error('Invalid tile input');
+  }
+
+  // Return the Unicode symbol for the tile
+  return unicode;
 }
 
 function ExpressDemo (props) {
@@ -59,10 +94,18 @@ function ExpressDemo (props) {
       if (!response) return;
 
       let obj = JSON.parse(response);
-      let tests = []
+      let query = obj.query;
+      let tests = obj.tests;
+      let results = obj.results;
+
+      let unicodeHand = ''
+      const split = query.split(' ');
+      split.forEach((tile) => unicodeHand+=(getMahjongUnicode(tile)));
+
       console.log(response);
       return <div className="express--response">
-        <p>Query: {obj.query}</p>
+        <p>Query: {unicodeHand}</p>
+        
         </div> ;
     }
   
