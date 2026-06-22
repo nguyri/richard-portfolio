9d3785aeea83d10c759ee5def7c2eb68ade72ef5
Richard Nguyen <11547131+nguyri@users.noreply.github.com>
2026-06-22 14:08:13 -0600

    Added dev mode for prod build in case

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,20 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const TerserPlugin = require('terser-webpack-plugin');
+const path = require('path');
 
 module.exports = merge(common, {
   mode: 'production',
+  // devServer block added so running webpack-dev-server with the
+  // production config will still fallback to index.html for SPA routes
+  // (useful when launching with --config webpack.prod.js)
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    port: 8080,
+    historyApiFallback: true,
+  },
   optimization: {
     // splitChunks: {
     //   chunks: 'all',
